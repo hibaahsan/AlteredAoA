4486c5a14f432471539e67ceec45fc7310884bca
Daivat Bhatt <daivatpbhatt@gmail.com>
2020-11-27 14:07:10 -0500

    Weights in topdown core for pointer gen

--- models/AttModel.py
+++ models/AttModel.py
@@ -506,6 +506,9 @@ class TopDownCore(nn.Module):
         self.att_lstm = nn.LSTMCell(opt.input_encoding_size + opt.rnn_size * 2, opt.rnn_size)  # we, fc, h^2_t-1
         self.lang_lstm = nn.LSTMCell(opt.rnn_size * 2, opt.rnn_size)  # h^1_t, \hat v
         self.attention = Attention(opt)
+        
+        #changes for pointer gen
+        self.attn_weights=None
 
     def forward(self, xt, fc_feats, att_feats, p_att_feats, state, att_masks=None):
         prev_h = state[0][-1]
@@ -514,7 +517,14 @@ class TopDownCore(nn.Module):
         h_att, c_att = self.att_lstm(att_lstm_input, (state[0][0], state[1][0]))
 
         att = self.attention(h_att, att_feats, p_att_feats, att_masks)
-
+        
+        #pointer gen changes
+        self.attn_weights = self.attention.attn
+        # add a dimension 1
+        self.attn_weights = torch.unsqueeze(self.attn_weights, 1)
+        # add a dimension for head
+        self.attn_weights = torch.unsqueeze(self.attn_weights, 1)
+        
         lang_lstm_input = torch.cat([att, h_att], 1)
         # lang_lstm_input = torch.cat([att, F.dropout(h_att, self.drop_prob_lm, self.training)], 1) ?????
 
@@ -616,6 +626,8 @@ class Attention(nn.Module):
         self.h2att = nn.Linear(self.rnn_size, self.att_hid_size)
         self.alpha_net = nn.Linear(self.att_hid_size, 1)
 
+        self.attn = None
+
     def forward(self, h, att_feats, p_att_feats, att_masks=None):
         # The p_att_feats here is already projected
         att_size = att_feats.numel() // att_feats.size(0) // att_feats.size(-1)
@@ -630,6 +642,8 @@ class Attention(nn.Module):
         dot = dot.view(-1, att_size)  # batch * att_size
 
         weight = F.softmax(dot, dim=1)  # batch * att_size
+        self.attn = weight
+
         if att_masks is not None:
             weight = weight * att_masks.view(-1, att_size).float()
             weight = weight / weight.sum(1, keepdim=True)  # normalize to 1
